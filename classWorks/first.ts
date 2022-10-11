d3c2f7f0ebfa9e75db224143809954e58c61dbc9
Abhilkv <abhilknight@gmail.com>
2022-10-11 21:23:58 +0530

    express basics

---
+++ classWorks/first.ts
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', (req, res) => {
+    res.send("First code on express");
+});
+
+var server = app.listen(8081, () => {
+    console.log('Server litening at 8081');
+})
